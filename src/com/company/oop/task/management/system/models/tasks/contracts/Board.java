4bdd6890aad3bcbbe775f5148591869a3d7a5a14
Viktor Angelov <vikotor05@gmail.com>
2024-11-11 23:45:54 +0200

    1. Create BoardImpl class 2. Create Board interface 3. Create BoardImplTests class 4. Create StoryImplTests class 5. Upgrade Story interface

---
+++ src/com/company/oop/task/management/system/models/tasks/contracts/Board.java
@@ -0,0 +1,15 @@
+package com.company.oop.task.management.system.models.tasks.contracts;
+
+import java.util.List;
+
+public interface Board {
+
+    String getName();
+
+    List<String> getTasks();
+
+    List<String> getActivityHistory();
+
+    void addTask(String task);
+
+}
--- src/com/company/oop/task/management/system/models/tasks/contracts/Story.java
+++ src/com/company/oop/task/management/system/models/tasks/contracts/Story.java
@@ -1,11 +1,18 @@
 package com.company.oop.task.management.system.models.tasks.contracts;
 
-import com.company.oop.task.management.system.models.tasks.enums.FeedbackStatus;
-import com.company.oop.task.management.system.models.tasks.enums.StoryStatus;
+import com.company.oop.task.management.system.models.tasks.enums.*;
 
 public interface Story extends Task {
 
+    Priority getPriority();
+
+    StorySize getSize();
+
     StoryStatus getStatus();
+
+    String getAssignee();
+
+    TaskType getType();
 
     void changeStatus(StoryStatus status);
 
