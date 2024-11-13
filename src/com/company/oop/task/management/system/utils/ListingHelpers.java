8f1c8a44c2c02c96e7e22cd23ce1541767f9db41
Ivan Ivanov <ivanovivanbusiness@gmail.com>
2024-11-13 13:47:36 +0200

    1. Created ListingHelpers for the future

---
+++ src/com/company/oop/task/management/system/utils/ListingHelpers.java
@@ -0,0 +1,18 @@
+package com.company.oop.task.management.system.utils;
+
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListingHelpers {
+//ToDo
+//    public static <T extends Printable> String elementsToString(List<T> elements) {
+//        List<String> stringElements = new ArrayList<>();
+//        for (T element : elements) {
+//            stringElements.add(element.getAsString());
+//        }
+//        return String.join(CommandsConstants.JOIN_DELIMITER + System.lineSeparator(), stringElements).trim();
+//    }
+
+}
