2431d7974d95519fd70e1e88e3f494a9bbba7a13
Viktor Angelov <vikotor05@gmail.com>
2024-11-20 17:58:58 +0200

    ChangePriorityOfBugCommand

--- src/com/company/oop/task/management/system/commands/modification/ChangePriorityOfBugCommand.java
+++ src/com/company/oop/task/management/system/commands/modification/ChangePriorityOfBugCommand.java
@@ -4,8 +4,8 @@ import com.company.oop.task.management.system.commands.BaseCommand;
 import com.company.oop.task.management.system.core.contracts.TaskManagementSystemRepository;
 import com.company.oop.task.management.system.models.tasks.contracts.Bug;
 import com.company.oop.task.management.system.models.tasks.enums.PriorityType;
-import com.company.oop.task.management.system.models.teams.contracts.Board;
 import com.company.oop.task.management.system.models.teams.contracts.Team;
+import com.company.oop.task.management.system.utils.ParsingHelpers;
 import com.company.oop.task.management.system.utils.ValidationHelpers;
 
 import java.util.List;
@@ -14,7 +14,7 @@ import static com.company.oop.task.management.system.commands.utils.CommandsCons
 
 public class ChangePriorityOfBugCommand extends BaseCommand {
 
-    public static final int EXPECTED_NUMBER_OF_ARGUMENTS = 4;
+    public static final int EXPECTED_NUMBER_OF_ARGUMENTS = 3;
     public static final String INVALID_PRIORITY_VALUE = "Invalid priority value: '%s'. Valid values are: LOW, MEDIUM, HIGH.";
 
     public ChangePriorityOfBugCommand(TaskManagementSystemRepository taskManagementSystemRepository) {
@@ -32,9 +32,8 @@ public class ChangePriorityOfBugCommand extends BaseCommand {
         ValidationHelpers.validateArgumentsCount(parameters, EXPECTED_NUMBER_OF_ARGUMENTS);
 
         String newPriority = parameters.get(0);
-        String bugName = parameters.get(1);
-        String boardName = parameters.get(2);
-        String teamName = parameters.get(3);
+        int bugId = ParsingHelpers.tryParseInt(parameters.get(1), INVALID_INPUT_MESSAGE);
+        String teamName = parameters.get(2);
 
         PriorityType priority;
         try {
@@ -48,27 +47,11 @@ public class ChangePriorityOfBugCommand extends BaseCommand {
             throw new IllegalArgumentException(String.format(NO_TEAMS_FOUND));
         }
 
-        Board board = team.getBoards()
-                .stream()
-                .filter(b -> b.getName().equalsIgnoreCase(boardName))
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException(
-                        String.format(NO_BOARDS_FOUND, teamName, teamName)
-                ));
-
-        Bug bug = board.getTasks()
-                .stream()
-                .filter(task -> task instanceof Bug)
-                .map(task -> (Bug) task)
-                .filter(b -> b.getTitle().equalsIgnoreCase(bugName))
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException(
-                        String.format(NO_REGISTERED_BUGS)
-                ));
+        Bug bug = getTaskManagementSystemRepository().findTaskById(getTaskManagementSystemRepository().getBugs(), bugId);
 
         bug.changePriority(priority);
 
-        bug.historyLogger(String.format(ENUM_CHANGED, bug.getId(), priority, newPriority));
+        bug.historyLogger(String.format(ENUM_CHANGED, bug.getId(), bug.getPriority(), newPriority));
         return String.format(BUG_PRIORITY_CHANGED);
     }
 }
